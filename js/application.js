29f0e109e6b5d9b345805795b637d710b3f56c84
Leigh Michael Forrest <leighmforrest@gmail.com>
2015-09-12 16:42:38 -0400

    Application ported to jquery API

--- js/application.js
+++ js/application.js
@@ -1,32 +1,11 @@
-var quiz_container = document.getElementById('quiz_container');
-var next_button = document.getElementById("next");
-var back_button =document.getElementById("back");
-//Remove the quiz from the quiz_container
-//quiz: Reference to the node containing the question
-function removeElement(node){
-	var removeEl = node;
-	var parentEl = removeEl.parentNode;
-	parentEl.removeChild(removeEl);
-}
-//inserts new question div in quiz_container
-function insertQuestion(quiz){
-	var newEl = document.createElement("div");
-	newEl.setAttribute('id','quiz');
-	quiz.appendChild(newEl);
-}
+$(function(){
+	//cache important elements
+	var quiz_container = $("#quiz_container");
+	var back_button = $("#back");
+	var next_button = $("#next");
 
-//replaces an old node with a new one
-//assumes quiz_container exists
-function replaceQuestion(){
-	//create a new node
-	var newNode = document.createElement("div");
-	newNode.setAttribute('id','quiz');
-	//get the old node. Assumes quiz_container is not null
-	var oldNode = quiz_container.lastChild;
-	quiz_container.replaceChild(newNode,oldNode);
-}
 //Draws a quesion onto a div
-//quiz: div element containing the question
+//quiz: jquery object containing the question
 //question: an object with a question
 function drawQuestion(quiz,question){
 	var frag= document.createDocumentFragment();
@@ -43,7 +22,6 @@ function drawQuestion(quiz,question){
 		answerSel.setAttribute('value', i);
 		if(Quiz.answerSheet[Quiz.getIterator()()] == i){
 			answerSel.setAttribute('checked','true');
-			console.log("Item " + i + " has been checked.")
 		}//end if
 		//add the label
 		var label= document.createElement("label");
@@ -53,87 +31,86 @@ function drawQuestion(quiz,question){
 		frag.appendChild(document.createElement("br"));
 	});
 	//append the fragment to the child
-	quiz.appendChild(frag);
+	quiz.prepend(frag);
 }
-
-//Get the value of the radio button group
-function getAnswersFromRadioButtons(){
-	var radio = document.getElementsByName('answer');
-	for(var i = 0; i < radio.length; i++){ 
-		if(radio[i].checked == true) return i; 
-		else continue;
+	//Get the value of the radio button group
+	function getAnswersFromRadioButtons(){
+		
+		var answer = parseInt($("#quiz input[type='radio']:checked").val());
+		if(!isNaN(answer)){ return answer;}else{return -1;}
+		
 	}
-	return -1;
-}
-
-//Shows back button if iterator is over 0; else hide button
-function showButton(){
-	if(Quiz.getIterator()() > 0){
-		back_button.className = '';
-	}else{
-		back_button.className = 'hideButton';
+	//Shows back button if iterator is over 0; else hide button; shows next button always
+	function showButtons(){
+		if(Quiz.getIterator()() > 0){
+			back_button.show();
+		}else{
+			back_button.hide();
+		}
 	}
 
-	next_button.className = '';
-}
+	//replace old quiz div with a fresh one
+	function replaceQuestion(){
+		quiz_container.find('#quiz').remove();
+  		quiz_container.append('<div id="quiz"></div>');
+	}
 
-/*
-Here are the event listeners
-onload->load the first question
-onclick->if iterator less than questions, replace the old question with the new
-	else, display the score
-*/
+	//event handlers
+	//Runs when window loads
+	$( window ).load(function() {
+		//remove fallback p and hideButton class
+  		next_button.removeClass('hideButton');
+  		back_button.removeClass('hideButton');
+  		replaceQuestion();
+  		drawQuestion($('#quiz'),Quiz.getQuestion());
+  		showButtons();
+	});
 
-EventUtil.addHandler(window,"load",function(event){
-	removeElement(quiz_container.lastChild);//remove the fallback <p> element
-	replaceQuestion();
-	drawQuestion(quiz_container.lastChild,Quiz.getQuestion());
-	showButton(); 
-});
-
-//The next_button event handler
-EventUtil.addHandler(next_button,"click",function(event){
-			var answer = getAnswersFromRadioButtons();
-			if(answer == -1){
+	//next button event handler
+	next_button.on('click', function(){
+		answer = getAnswersFromRadioButtons();
+		if(answer == -1){
 				alert("You cannot proceed unless the question is answered.");
 			}else{
 			Quiz.answerQuestion(answer);
-			replaceQuestion();//replace old question div with a blank one
+			//quiz_container.replaceWith('<div id="quiz"></div>');
+			//replace old question div with a blank one
 			//if(Quiz.getIterator() < Quiz.allQuestions.length){
 			if(Quiz.moreQuestions()){
-				drawQuestion(quiz_container.lastChild,Quiz.getQuestion());
-				showButton();
+				replaceQuestion();
+				drawQuestion($('#quiz'),Quiz.getQuestion());
+				showButtons();
 			}else{
 				//score the quiz
 				var score = Quiz.scoreQuiz();
 				//use removeQuestion function to remove buttons
-				removeElement(next_button.parentNode);
+				$('#quiz_button').remove();
 				//insert paragraph to display score
-				var scoreDisp = document.createElement("p");
-				var scoreDispTxt = document.createTextNode("Your score is " + score);
-				scoreDisp.appendChild(scoreDispTxt);
-				quiz_container.lastChild.appendChild(scoreDisp);
+				var scoreDispTxt = "Your score is " + score;
+				replaceQuestion();
+				$("#quiz").append("<p>"+ scoreDispTxt +"</p>");
 			}
 		}
-});
+	});
 
-//The back_button event handler
-EventUtil.addHandler(back_button,"click",function(event){
-	var iterator = Quiz.getIterator()();
-	if(iterator > -1 ){
-		//if it is the last question, remember the button
-		//Score the answer before decrementing iterator
-		if(iterator == (Quiz.allQuestions.length-1))
-		{
-			var answer = getAnswersFromRadioButtons();
-			Quiz.answerSheet[iterator] = answer;
-
+	//back button event handler
+	back_button.on('click', function(){
+		var iterator = Quiz.getIterator()();
+		if(iterator > -1 ){
+			//if it is the last question, remember the button
+			//Score the answer before decrementing iterator
+			if(iterator == (Quiz.allQuestions.length-1))
+			{
+				var answer = getAnswersFromRadioButtons();
+				Quiz.answerSheet[iterator] = answer;
+			}
+			//decrement iterator
+			Quiz.decrementIterator();
+			//draw question
+			replaceQuestion();
+			drawQuestion($('#quiz'),Quiz.getQuestion());
+			
+			showButtons();
 		}
-		//decrement iterator
-		Quiz.decrementIterator();
-		//draw question
-		replaceQuestion();
-		drawQuestion(quiz_container.lastChild,Quiz.getQuestion());
-		showButton();
-	}
+	});
 });
